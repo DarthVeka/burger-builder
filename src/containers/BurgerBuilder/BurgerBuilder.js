e5de74226cd0372e421e569d1d5ba598c34e7aff
Darth.Veka <vedran.von.vitez@gmail.com>
2018-02-09 14:55:00 +0100

    outputing visual representation of our burger using state

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends React.Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2          
+        }
+    }
+    render() {
+        return(
+            <React.Fragment>
+                <Burger ingredients={this.state.ingredients} />
+                <div>Build Controls</div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default BurgerBuilder;
