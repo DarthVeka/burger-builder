e18f9ab8920bc751c6d97e04346c8e354ef55054
Darth.Veka <vedran.von.vitez@gmail.com>
2018-03-12 14:19:22 +0100

    Added Auth page

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+import classes from './Auth.css';
+
+class Auth extends Component {
+
+    state = {
+        controls: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your Email'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    email: true
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                touched: false
+            }
+        }
+    }
+
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        if (rules.email) {
+            const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            isValid = re.test(String(value).toLowerCase()) && isValid;
+        }
+
+        return isValid;
+    }
+
+    inputChangedHandeler = (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName] : {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                touched: true
+            }
+        }
+        this.setState({ controls: updatedControls });
+    }
+
+    render() {
+
+        const formElementsArray = [];
+        for (let key in this.state.controls) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.controls[key]
+            })
+        }
+
+        const form = formElementsArray.map(formElem => (
+            <Input
+                validity={formElem.config.valid}
+                touched={formElem.config.touched}
+                key={formElem.id}
+                elementType={formElem.config.elementType}
+                elementConfig={formElem.config.elementConfig}
+                value={formElem.config.value}
+                changed={(event) => this.inputChangedHandeler(event, formElem.id)}
+            />
+        ));
+
+        return (
+            <div className={classes.Auth}>
+                <h3>Log In</h3>
+                <form>
+                    {form}
+                    <Button btnType='Success'>Submit</Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Auth;
