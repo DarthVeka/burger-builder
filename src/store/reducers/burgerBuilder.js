a3a5715ba97c45a74bcca6629c669aa70b344829
Darth.Veka <vedran.von.vitez@gmail.com>
2018-03-12 10:10:38 +0100

    Refactored reducers

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const INGREDIENT_PRICES = {
     salad: 0.2,
@@ -16,25 +17,23 @@ const initialState = {
 const burgerBuilderReducer = (state = initialState, action) => {
     switch (action.type) {
 
-        case actionTypes.ADD_INGREDIENT:        
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-                },
+        case actionTypes.ADD_INGREDIENT:
+            const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 };
+            const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+            const updatedState = {
+                ingredients: updatedIngredients,
                 totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            };
+            }
+            return updatedState;
 
         case actionTypes.REMOVE_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-                },
+            const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 };
+            const updatedIngs = updateObject(state.ingredients, updatedIng);
+            const updatedSt = {
+                ingredients: updatedIngs,
                 totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
-            };
+            }
+            return updatedSt;
 
         case actionTypes.CLEAR_STATE:
             return {
@@ -44,23 +43,20 @@ const burgerBuilderReducer = (state = initialState, action) => {
                 }
             };
 
-            case actionTypes.SET_INGREDIENTS:
-            return {
-                ...state,
+        case actionTypes.SET_INGREDIENTS:
+            return updateObject(state, {
                 ingredients: {
                     salad: action.ingredients.salad,
                     bacon: action.ingredients.bacon,
                     cheese: action.ingredients.cheese,
                     meat: action.ingredients.meat
                 },
-                totalPrice: 4
-            };
+                totalPrice: 4,
+                error: false
+            });
 
-            case actionTypes.FETCH_INGREDIENTS_FAILED:
-            return {
-                ...state,
-                error: true
-            };
+        case actionTypes.FETCH_INGREDIENTS_FAILED:
+            return updateObject(state, { error: true });
 
         default:
             return state;
